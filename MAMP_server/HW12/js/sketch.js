418bb0da7db45f4a4e25ffebe98601aff47c184a
Oleg <olgarshin@gmail.com>
2019-12-03 16:51:35 +0700

    'hm13_errorHandling'

--- MAMP_server/HW12/js/sketch.js
+++ MAMP_server/HW12/js/sketch.js
@@ -131,6 +131,74 @@ window.addEventListener('DOMContentLoaded', function() { //после загру
 
     //Form ---------------------серверные запросы------------------------------------------------------------------
 
+    // let message = {
+    //     loading: "Загрузка...",
+    //     sucsess: "Спасибо! Скоро мы с Вами свяжемся!",
+    //     failure: "Что-то пошло не так..."
+    // };
+    
+    // let form = document.querySelector('.main-form'),
+    //     formDown = document.querySelector('#form'),
+    //     input = form.getElementsByTagName ('input'),
+    //     statusMessage = document.createElement('div');
+         
+    
+    //     statusMessage.classList.add('status');
+    
+    // function sendForm(elem){
+    
+    //     elem.addEventListener('submit', function (event) {
+    //         event.preventDefault();
+    //         elem.appendChild(statusMessage);
+        
+    //         let formData = new FormData(elem);
+        
+    //     function postData() {
+    //         return new Promise(function (resolve, reject) {
+    //             let request = new XMLHttpRequest();
+    //             request.open('POST', 'server.php');
+    //             request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        
+    //             request.onreadystatechange = function () {
+    //                 if(request.readyState < 4) {
+    //                     resolve();
+    //                 } else if (request.readyState === 4) {
+    //                     if (request.status == 200 && request.status < 300) {
+    //                         resolve();
+    //                     } else {
+    //                         reject();
+    //                     }
+    //                 }
+    //             };
+        
+    //             let obj = {};
+    //             formData.forEach (function (value, key) {
+    //                 obj[key] = value;
+    //             });
+    //             let json = JSON.stringify(obj);
+    //             request.send(json);
+ 
+    //         });
+    //     }
+        
+    //     function clearInput() {
+    //         for (let i = 0; i < input.length; i++) {
+    //             input[i].value = '';
+    //         }
+    //     }
+        
+    //     postData (formData)
+    //         .then (() => statusMessage.innerHTML = message.loading)
+    //         .then (() => statusMessage.innerHTML = message.sucsess)
+    //         .catch (() => statusMessage.innerHTML = message.failure)
+    //         .then (clearInput);
+    //     });
+ 
+    // }
+    
+    // sendForm(form);
+    // sendForm(formDown);
+
     //объект с сообщениями выполнения запросов
     let message = { 
         loading: 'Загрузка...',
@@ -141,28 +209,16 @@ window.addEventListener('DOMContentLoaded', function() { //после загру
     //работаем с формой обратной связи 
     let form = document.querySelector('.main-form'), //сама форма
         input = form.getElementsByTagName('input'), //поле ввода
-
-        //по заданию тоже самое, только для контактной формы
-        contactForm = document.querySelector('#form'), //сама контакная форма(та, что снизу)
-        contactInput = contactForm.getElementsByTagName('input'), //поле ввода для контакной формы
-        
         statusMessage = document.createElement('div'); //новый раздел, где будем писать о статусе запроса(снизу формы)
         statusMessage.classList.add('status'); //добавляем класс СТАТУС к новому разделу в форме
-        //--------------------------------------------------------------------------------------------------------
+
         //функция для очистки полей
         function clearInput(){
-            for (let i = 0; i > input.length; i++) { 
-                input[i].value = ''
-                alert('Поля очистились!');
+            for (let i = 0; i < input.length; i++) { 
+                input[i].value = '';
             }
-        }
-
-        function clearInputContact(){
-            for (let i = 0; i > input.length; i++) { 
-                contactInput[i].value = ''
-                alert('Поля очистились!');
-            }
-        }
+            alert('Поля очистились!');
+        }    
 
         form.addEventListener('submit', function(event){ //при нажатии на отправить - форма отправляется
             
@@ -196,22 +252,23 @@ window.addEventListener('DOMContentLoaded', function() { //после загру
                     }
                 }
             });
+        }); //конец промиса            
 
-        }); //конец промиса
-            
             } //конец функции smartRequest 
 
-            smartRequest()   //запускаем функцию smartRequest
-            .then(() => statusMessage.innerHTML = message.success) //в  случае успеха промиса
-            .then(clearInput()) //чистим поля
-            .catch(() => statusMessage.innerHTML = message.failure) //в  случае провала промиса
             
-            //.finally(() => alert('сработало finally!')) 
 
+            smartRequest(form)   //запускаем функцию smartRequest
+            .then(() => statusMessage.innerHTML = message.success) //в случае успеха промиса
+            .then(clearInput) //чистим поля
+            .catch(() => statusMessage.innerHTML = message.failure); //в случае провала промиса
+            //clearInput(); 
     }); //конец прослушивателя формы
 
     //по заданию для контактной формы !!! -------------------!---------------------------------
-
+    let contactForm = document.querySelector('#form'), //сама контакная форма(та, что снизу)
+        contactInput = contactForm.getElementsByTagName('input'); //поле ввода для контакной формы
+    
     contactForm.addEventListener('submit', function(event){ //при нажатии на отправить - форма отправляется
         event.preventDefault(); //чтобы страница не обновлялась(убираем стандартное поведение браузера)
         
@@ -248,47 +305,23 @@ window.addEventListener('DOMContentLoaded', function() { //после загру
         }); //конец промиса
             
             } //конец функции smartRequest 
+            
+            function clearInputContact(){
+                for (let i = 0; i < contactInput.length; i++) { 
+                    contactInput[i].value = ''
+                }
+                alert('Поля очистились!');
+            }
 
             smartRequestContact()   //запускаем функцию smartRequest
             .then(() => statusMessage.innerHTML = message.success) //в  случае успеха промиса
-            .then(clearInputContact()) //чистим поля
-            .catch(() => statusMessage.innerHTML = message.failure) //в  случае провала промиса
-            
+            .then(clearInputContact) //чистим поля
+            .catch(() => statusMessage.innerHTML = message.failure); //в  случае провала промиса
+            //clearInput();
             //.finally(() => alert('сработало finally!')) 
 
     }); //конец прослушивателя формы
 
-    //     let request = new XMLHttpRequest(); //начинаем описывать и отправлять запрос
-    //     request.open('POST', 'server.php');
-    //     request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    //     let formData = new FormData(contactForm); //создаем объект formData
-
-    //     //превращаем formData в JSON-объект
-    //     let obj = {}; //создаем пустой JS-объект
-    //     formData.forEach(function(value, key){
-    //         obj[key] =  value;  //методом forEach вносим из formData в пустой JS-объект значения
-    //     });
-    //     let json = JSON.stringify(obj); //превращаем наш JS-объект в JSON-объект
-
-    //     request.send(json); //отправляем JSON-объект
-
-    //     request.addEventListener('readystatechange', function(){ //показ в сообщении статуса запроса
-    //         if (request.readyState < 4) {
-    //             statusMessage.innerHTML = message.loading; //"загрузка", если не загрузился
-    //         } else if (request.readyState === 4 && request.status == 200) {
-    //             statusMessage.innerHTML = message.success; //"успех", если загрузился
-    //         } else {
-    //             statusMessage.innerHTML = message.failure; //"ошибка", если произошла ошибка
-    //         }
-    //     });
-
-    //     //очищаем все поля ввода после отправки запроса
-    //     for (let i = 0; i > contactInput.length; i++) { 
-    //         contactInput[i].value = '';
-    //     }
-
-    // });
 });
 
 
